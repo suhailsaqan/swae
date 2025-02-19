032cda745c7c4c9e9e51e7e2f815572798e977f7
Suhail Saqan <suhail.saqan@gmail.com>
2025-02-18 23:12:55 -0500

    recreate tab structure and player view and add live chat

--- swae/VideoPlayer/CustomVideoPlayer.swift
+++ swae/VideoPlayer/CustomVideoPlayer.swift
@@ -10,15 +10,42 @@ import SwiftUI
 
 struct CustomVideoPlayer: UIViewControllerRepresentable {
     var player: AVPlayer
+    @Binding var videoSize: CGSize
+
     func makeUIViewController(context: Context) -> AVPlayerViewController {
         let controller = AVPlayerViewController()
         controller.player = player
         controller.showsPlaybackControls = false
 
+        // Observe video size
+        if let item = player.currentItem {
+            item.addObserver(context.coordinator, forKeyPath: "presentationSize", options: .new, context: nil)
+        }
+
         return controller
     }
 
-    func updateUIViewController(_ uiViewController: AVPlayerViewController, context: Context) {
+    func updateUIViewController(_ uiViewController: AVPlayerViewController, context: Context) {}
 
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
+
+    class Coordinator: NSObject {
+        var parent: CustomVideoPlayer
+
+        init(_ parent: CustomVideoPlayer) {
+            self.parent = parent
+        }
+
+        override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+            if keyPath == "presentationSize",
+               let item = object as? AVPlayerItem,
+               let newSize = change?[.newKey] as? CGSize {
+                DispatchQueue.main.async {
+                    self.parent.videoSize = newSize
+                }
+            }
+        }
     }
 }
